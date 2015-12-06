6d66460c9fbb15f080af40b8f7f64a3d6c6e0fb9
Jason Antman <jason@jasonantman.com>
2015-12-06 14:12:18 -0500

    puppet to install dependencies

--- README.rst
+++ README.rst
@@ -2,3 +2,14 @@ piface-webhooks
 ===============
 
 Python script/daemon to read RPi PiFace inputs and run a webhook when they change.
+
+Installation
+-------------
+
+This assumes a Debian-based machine, like `Raspbian <>`_ or `OSMC <>`_. Run this as root:
+
+1. ``apt-get update && apt-get install -y puppet git``
+2. ``cd /usr/local && git clone https://github.com/jantman/piface-webhooks.git && cd piface-webhooks``
+3. ``puppet apply piface_webhooks_dependencies.pp`` - this will install all system-level dependencies (python3, virtualenv, pip)
+4. ``puppet apply piface_webhooks_setup.pp`` - this will create the virtualenv, install the package and set it up to run as a service
+5. 
---
+++ piface_webhooks_dependencies.pp
@@ -0,0 +1,44 @@
+# piface_webhooks_dependencies.pp
+#
+# Puppet manifest to install system-level dependencies for piface-webhooks
+#
+# The latest version of this package is available at:
+# <https://github.com/jantman/piface_webhooks>
+#
+################################################################################
+# Copyright 2015 Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+#
+#    This file is part of piface_webhooks.
+#
+#    piface_webhooks is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    piface_webhooks is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with piface_webhooks.  If not, see <http://www.gnu.org/licenses/>.
+#
+# The Copyright and Authors attributions contained herein may not be removed or
+# otherwise altered, except to add the Author attribution of a contributor to
+# this work. (Additional Terms pursuant to Section 7b of the AGPL v3)
+################################################################################
+# While not legally required, I sincerely request that anyone who finds
+# bugs please submit them at <https://github.com/jantman/piface_webhooks> or
+# to me via email, and that you send any contributions or improvements
+# either as a pull request on GitHub, or to me via email.
+################################################################################
+#
+# AUTHORS:
+# Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+################################################################################
+
+$packages = ['python3', 'python3-pip', 'python3-virtualenv']
+
+package { $packages:
+  ensure => present,
+}
